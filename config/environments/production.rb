3adc5f437112b976763385114c41ab4469f6aee3
Katsuhisa Takeda <Katsuhisa12@hotmail.com>
2017-02-13 14:40:51 +0900

    devise導入

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #本番環境ではlocalhostを変更する
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  root :to => "lists#index"
+
+  devise_for :users
+
   resources :items
   resources :lists
-  get 'done' => 'items#done' 
+  get 'done' => 'items#done'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
